340356cd160be64e8927f832a6cac17f5b75af78
Emilion <emiljadersten@gmail.com>
2024-03-12 11:42:17 +0100

    More adjustments for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -59,7 +59,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://emils-drf-api-8546c6776dcb.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
