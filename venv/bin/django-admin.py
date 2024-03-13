de748e4e92d76b252eb18d296211f7b417e261e5
Emilion <emiljadersten@gmail.com>
2024-03-13 15:58:22 +0100

    Create new venv with python3.8

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/emil/Skrivbord/web-projects/emils-drf-api/venv/bin/python
+#!/home/emil/Skrivbord/web-projects/emils-drf-api/venv/bin/python3.8
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
